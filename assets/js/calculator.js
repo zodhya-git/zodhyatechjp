486dec1a58f4ad4e393a8d40cbece22708695f2e
amit <adhikariamit2001@gmail.com>
2023-01-28 21:00:09 +0530

    UI fix

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -3,6 +3,10 @@ thisForm.addEventListener('submit', async function (e) {
   e.preventDefault();
   const formData = new FormData(thisForm).entries()
   try {
+    
+    document.querySelector("#alert").style.display = "none"
+    document.querySelector("#alert-contact").style.display = "none"
+    
     document.querySelector(".preload").style.display = "block"
     init();
     const response = await fetch('https://zodhyatech.pythonanywhere.com/calculate', {
@@ -16,7 +20,11 @@ thisForm.addEventListener('submit', async function (e) {
     document.querySelector(".preload").style.display = "none"
     var saver =result[0]
     saver=Math.ceil(saver/100)*100
-    document.getElementById("alert").innerHTML = `Current Energy Consumption INR ${saver} .\nUsing Zodhya device you can save upto INR ${saver*0.25} - ${saver*0.35} `
+
+    document.querySelector("#alert").style.display = "block"
+    document.querySelector("#alert-contact").style.display = "block"
+    
+    document.getElementById("alert").innerHTML = `Using Zodhya device you can save upto INR ${saver*0.25} - INR ${saver*0.35} `
     document.getElementById("alert-contact").innerHTML = `For more info.<a href="https://zodhyatech.com/contact.html">Contact Us</a>`
     thisForm.reset();
 
