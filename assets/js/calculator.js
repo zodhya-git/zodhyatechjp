eaa541aebfa83a1c426568d6b2205c2d2da9a1c2
amit <adhikariamit2001@gmail.com>
2023-02-01 22:38:12 +0530

    Back4app added

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -2,6 +2,7 @@ const thisForm = document.getElementById('calculatorForm');
 thisForm.addEventListener('submit', async function (e) {
   e.preventDefault();
   const formData = new FormData(thisForm).entries()
+
   try {
 
     document.querySelector("#alert").style.display = "none"
@@ -9,16 +10,18 @@ thisForm.addEventListener('submit', async function (e) {
 
     document.querySelector(".preload").style.display = "block"
     init();
+
+    
     const response = await fetch('https://zodhyatech.pythonanywhere.com/calculate', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(Object.fromEntries(formData))
     });
-
-
+    
     const result = await response.json();
     document.querySelector(".preload").style.display = "none"
     var saver = result[0]
+    
     saver = Math.ceil(saver / 100) * 100
 
     document.querySelector("#alert").style.display = "block"
@@ -33,11 +36,13 @@ thisForm.addEventListener('submit', async function (e) {
 
 });
 
+
+
+
 const loader = document.querySelector('.preload');
 const emoji = loader.querySelector('.emoji');
 
-const emojis = ["🕐", "🕜", "🕑", "🕝", "🕒", "🕞", "🕓", "🕟", "🕔", "🕠", "🕕", "🕡", "🕖", "🕢", "🕗", "🕣", "🕘", "🕤", "🕙", "🕥", "🕚", "🕦", "🕛", "🕧"];
-
+const emojis = ["🕐", "🕑", "🕒", "🕓", "🕔", "🕔", "🕓", "🕕", "🕔", "🕠", "🕕", "🕡", "🕖", "🕢", "🕗", "🕣", "🕘", "🕤", "🕙", "🕥", "🕚", "🕦", "🕛", "🕧"];
 const interval = 125;
 
 const loadEmojis = (arr) => {
