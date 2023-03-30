98d4a66b1b35e9513bf3b449135347e7b8a9f167
amit <adhikariamit2001@gmail.com>
2023-03-30 16:31:28 +0530

    optimal calculator added

---
+++ assets/js/predict.js
@@ -0,0 +1,46 @@
+  const optimalForm = document.getElementById('optimalForm');
+  optimalForm.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    // const result = getFormJSON(optimalForm);
+
+    const formData = new FormData(optimalForm).entries()
+    try {
+      const response = await fetch('http://yashwanthzodhya.pythonanywhere.com/predict_api', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(Object.fromEntries(formData))
+      });
+  
+      const result = await response.json();
+      console.log(result)
+
+      
+    mode_object = {
+        0:'auto',
+        1:'fan',
+        2:'dry',
+        3:'cool',
+    }
+
+    fan_speed_object = {
+        0:'auto',
+        1:'quiet',
+        2:'low',
+        3:'medium',
+        4:'high',
+    }
+    
+    let mode_setting=mode_object[result.output1]
+    let setpoint_setting=result.output2
+    let fan_setting= fan_speed_object[result.output3]
+    
+    let showdata=`Predicted AC Settings: Mode: ${mode_setting}, Setpoint: ${setpoint_setting}.0, Fan: ${fan_setting}`
+    document.getElementById('optimalresult').innerHTML=showdata
+    } catch (error) {
+      console.log(error);
+    }
+  
+  });
+  
+  
+  
