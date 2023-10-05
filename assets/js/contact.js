71817c555b52ba2109c55f74a75c11af22073ff5
adhikari-amit <adhikariamit2001@gmail.com>
2023-10-05 10:34:44 +0530

    jp saver

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,10 +3,10 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
     emailjs.sendForm('service_buj4qoj', 'template_kc16rla', '#contact-form')
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
-            document.getElementById("success").innerHTML = "We got your message. Will contact you as soon as possible."
+            document.getElementById("success").innerHTML = "できるだけ早くご連絡いたします。"
             document.getElementById('contact-form').reset();
         }, function (error) {
-            document.getElementById("alert").innerHTML = "Something Went Wrong !!!. Please Contact via other social Handels."
+            document.getElementById("alert").innerHTML = "何かが間違っていた！。 他のソーシャル・ハンデルを通じてご連絡ください。"
             console.log('FAILED...', error);
         });
 
